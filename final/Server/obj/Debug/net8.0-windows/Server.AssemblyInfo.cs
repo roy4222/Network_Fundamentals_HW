0a9bc392e0bc16773b0971f71734a934374801d8
roy4222 <roy422roy@gmail.com>
2025-06-18 21:38:47 +0800

    完成第一階段修正

--- final/Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
+++ final/Server/obj/Debug/net8.0-windows/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b434710fb241f93db5da8cb12388e51998dfb60e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68cae3278e3c352886cc7fe1d412cca0242813f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
